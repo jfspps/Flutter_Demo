7ff7c522f097062d0f5269011bd2b5d070ea0a14
James Apps <james.apps@energise.energy>
2023-07-27 15:37:27 +0100

    defined classes and instantiated a custom widget (object)

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   // pass the MaterialApp widget https://api.flutter.dev/flutter/material/MaterialApp-class.html
 
-  // nest widgets, with MaterialApp being the parent widget;
-  // also demonstrated are named arguments (as opposed to position arguments found in C based languages)
-  runApp(MaterialApp(
-      home: Scaffold(
-          body: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Colors.green, Colors.brown],
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft)),
-              child: const Center(
-                  child: Text('Roll the dice',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24)))))));
+  runApp(const CustomWidget());
+}
+
+class CustomWidget extends StatelessWidget {
+  // this is needed for all widgets; params in curly braces are named arguments;
+  // here we pass the Widget key to the StatelessWidget parent instance field "key"
+  const CustomWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // nest widgets, with MaterialApp being the parent widget;
+    // also demonstrated are named arguments (as opposed to position arguments found in C based languages)
+    return MaterialApp(
+        home: Scaffold(
+            body: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.green, Colors.brown],
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft)),
+                child: const Center(
+                    child: Text('Roll the dice',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24))))));
+  }
 }
