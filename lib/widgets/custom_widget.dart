4eac61be0a065ea24c997420cc51832c330f315e
James Apps <james.apps@energise.energy>
2023-07-27 15:44:59 +0100

    migrated class to dedicated file

---
+++ lib/widgets/custom_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomWidget extends StatelessWidget {
+  // this is needed for all widgets; params in curly braces are named arguments;
+  // here we pass the Widget key to the StatelessWidget parent instance field "key"
+  const CustomWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // nest widgets, with MaterialApp being the parent widget;
+    // also demonstrated are named arguments (as opposed to position arguments found in C based languages)
+    return MaterialApp(
+        home: Scaffold(
+            body: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.green, Colors.brown],
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft)),
+                child: const Center(
+                    child: Text('Roll the dice',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24))))));
+  }
+}
