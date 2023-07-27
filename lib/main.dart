32ed28416afb217871bc95aafff93643549708d4
James Apps <james.apps@energise.energy>
2023-07-27 11:40:51 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // pass the MaterialApp widget https://api.flutter.dev/flutter/material/MaterialApp-class.html
+
+  // set home field to Text widget as part of the MaterialApp constructor
+  runApp(MaterialApp(home: Text('Roll the dice')));
+}
