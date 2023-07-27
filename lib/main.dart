717abf584717b938f59a3b769abb483570b238ac
James Apps <james.apps@energise.energy>
2023-07-27 12:12:27 +0100

    nested widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   // pass the MaterialApp widget https://api.flutter.dev/flutter/material/MaterialApp-class.html
 
-  // set home field to Text widget as part of the MaterialApp constructor
-  runApp(MaterialApp(home: Text('Roll the dice')));
+  // nest widgets, with MaterialApp being the parent widget
+  runApp(const MaterialApp(
+      home: Scaffold(
+          body: Center(
+              child: Text('Roll the dice')
+          )
+      )
+  ));
 }
