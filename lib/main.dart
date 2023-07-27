61233398eccc26233b2a3a0c9835004eb050cdb4
James Apps <james.apps@energise.energy>
2023-07-27 14:45:26 +0100

    nested widgets, with named arguments in action

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   // pass the MaterialApp widget https://api.flutter.dev/flutter/material/MaterialApp-class.html
 
-  // nest widgets, with MaterialApp being the parent widget
-  runApp(const MaterialApp(
+  // nest widgets, with MaterialApp being the parent widget;
+  // also demonstrated are named arguments (as opposed to position arguments found in C based languages)
+  runApp(MaterialApp(
       home: Scaffold(
-          body: Center(
-              child: Text('Roll the dice')
-          )
-      )
-  ));
+          body: Container(
+              decoration: const BoxDecoration(
+                  gradient:
+                      LinearGradient(colors: [Colors.white24, Colors.brown])),
+              child: const Center(child: Text('Roll the dice'))))));
 }
