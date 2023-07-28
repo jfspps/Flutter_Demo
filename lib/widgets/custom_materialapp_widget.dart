9f9bdf8199606c8cd4d96366827028a36871d3af
James Apps <james.apps@energise.energy>
2023-07-28 10:08:55 +0100

    impl rollDice function

--- lib/widgets/custom_materialapp_widget.dart
+++ lib/widgets/custom_materialapp_widget.dart
@@ -5,14 +5,21 @@ import 'package:flutter/material.dart';
 const double defaultSize = 24;
 const String message = "Roll the dice";
 
-const String defaultImageName = "dice-1.png";
-
 /// Custom MaterialApp widget that applies a top-right to bottom-left gradient, with the
 /// CustomText widget styling in place.
 class CustomMaterialAppWidget extends StatelessWidget {
   // this is needed for all widgets; params in curly braces are named arguments;
   // here we pass the Widget key to the StatelessWidget parent instance field "key"
-  const CustomMaterialAppWidget({super.key});
+  CustomMaterialAppWidget({super.key});
+
+  String imageFilename = "dice-1.png";
+
+  // define function here, in preference to using anonymous functions (similar to
+  // lambda functions)
+  void rollDice() {
+    // this does nothing
+    imageFilename = "dice-2.png";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +35,10 @@ class CustomMaterialAppWidget extends StatelessWidget {
                         colors: [Colors.green, Colors.brown],
                         begin: Alignment.topRight,
                         end: Alignment.bottomLeft)),
-                child: const Center(
-                    child: CustomImageWidget(defaultImageName)))));
+                child: Center(
+                    child: Column(children: [
+                  CustomImageWidget(imageFilename),
+                  TextButton(onPressed: rollDice, child: const Text(message))
+                ])))));
   }
 }
